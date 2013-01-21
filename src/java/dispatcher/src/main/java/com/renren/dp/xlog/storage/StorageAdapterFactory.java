bdecd7ef2fe75647b779410fe8d493f066b1271b
xianquan.zhang <xianquan.zhang@renren-inc.com>
2013-01-21 18:42:37 +0800

    修改git pull后覆盖的类

--- src/java/dispatcher/src/main/java/com/renren/dp/xlog/storage/StorageAdapterFactory.java
+++ src/java/dispatcher/src/main/java/com/renren/dp/xlog/storage/StorageAdapterFactory.java
@@ -10,12 +10,11 @@ public class StorageAdapterFactory {
 		StorageAdapter storageAdapter=null;
 		String storageType=Configuration.getString("storage.type","");
 		String storageVersion=Configuration.getString("storage.version","");
-		int bufferSize=Configuration.getInt("hdfs.buffer.size", 4000);
 		if(storageType.equals("hdfs")){
 			if(storageVersion.equals("1.0.3")){
-				storageAdapter=new HDFSAdapter1_0_3(Configuration.getString("xlog.uuid"),bufferSize);
+				storageAdapter=new HDFSAdapter1_0_3();
 			}else if(storageVersion.equals("0.21")){
-				storageAdapter=new HDFSAdapter0_21(Configuration.getString("xlog.uuid"),bufferSize);
+				storageAdapter=new HDFSAdapter0_21();
 			}
 		}
 		return storageAdapter;
