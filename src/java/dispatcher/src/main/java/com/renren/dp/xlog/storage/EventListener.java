c03f6293d0e3e93376ce9423a3346eb695455eef
xianquan.zhang <xianquan.zhang@renren-inc.com>
2013-01-25 09:58:24 +0800

    将log队列中的notify()移除判断条件之外，即是不管队列是否满都会通知

--- src/java/dispatcher/src/main/java/com/renren/dp/xlog/storage/EventListener.java
+++ src/java/dispatcher/src/main/java/com/renren/dp/xlog/storage/EventListener.java
@@ -82,8 +82,8 @@ public class EventListener extends Thread {
         logMeta = null;
       } else {
         logBQ.add(logMeta);
-        logBQ.notify();
       }
+      logBQ.notify();
     }
   }
 
