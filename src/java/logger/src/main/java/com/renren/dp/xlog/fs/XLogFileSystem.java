d82d54056938bbdd6b0fd7ff0a7177c371c09827
Zhancheng Deng <zhancheng.deng@renren-inc.com>
2013-04-10 16:04:52 +0800

    change the XLogReader to interface

--- src/java/logger/src/main/java/com/renren/dp/xlog/fs/XLogFileSystem.java
+++ src/java/logger/src/main/java/com/renren/dp/xlog/fs/XLogFileSystem.java
@@ -1,6 +1,13 @@
 package com.renren.dp.xlog.fs;
 
+import java.io.Closeable;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * xlog file system simple example:
@@ -12,18 +19,21 @@ import java.io.IOException;
  * while ((line = reader.readLine()) != null) {
  *   System.out.println(line);
  * }
+ * reader.close();
+ * xfs.close();
  * </pre>
  * 
  * @author Zhancheng Deng {@mailto: zhancheng.deng@renren-inc.com}
  * @since 3:15:56 PM Apr 7, 2013
  */
-public class XLogFileSystem {
+public class XLogFileSystem implements Closeable {
 
   public static String ZK_ROOT_PATH = "/xlog/dispatchers";
   public static String DEFAULT_ZK_CLUSTER_IPS = "10.4.19.78:10060,10.4.19.79:10060,10.4.19.80:10060";
 
   private String zkConnectString;
   private XLogConfiguration conf;
+  private List<XLogReader> readerList;
 
   /**
    * default constructure
@@ -48,6 +58,7 @@ public class XLogFileSystem {
   public XLogFileSystem(String zkConnectString, XLogConfiguration conf) {
     this.zkConnectString = zkConnectString;
     this.conf = conf;
+    readerList = new CopyOnWriteArrayList<XLogReader>();
   }
 
   /**
@@ -58,7 +69,49 @@ public class XLogFileSystem {
    * @throws IOException
    */
   public XLogReader open(String[] categories) throws IOException {
-    return new XLogReader(categories, zkConnectString + ZK_ROOT_PATH, conf);
+    return new XLogReaderImpl(categories, zkConnectString + ZK_ROOT_PATH, conf);
   }
 
+  @Override
+  public void close() throws IOException {
+    for (XLogReader reader : readerList) {
+      reader.close();
+    }
+    readerList.clear();
+    readerList = null;
+  }
+
+  class XLogReaderImpl implements XLogReader {
+
+    private final Logger LOG = LoggerFactory.getLogger(XLogReaderImpl.class);
+
+    private DispatcherClusterReader client;
+
+    /**
+     * the xlog reader constructor
+     * 
+     * @param categories
+     * @param zkConnString
+     * @param options
+     * @throws IOException
+     */
+    public XLogReaderImpl(String[] categories, String zkConnString, XLogConfiguration conf) throws IOException {
+      client = new DispatcherClusterReader(categories, zkConnString, conf);
+      readerList.add(this);
+      LOG.info("Initialize the xlogInputStream (categories=" + Arrays.toString(categories) + ", zkConnString="
+          + zkConnString + ") successfully.");
+    }
+
+    @Override
+    public String readLine() throws IOException {
+      return client.readLine();
+    }
+
+    @Override
+    public void close() throws IOException {
+      client.close();
+      readerList.remove(this);
+    }
+
+  }
 }
--- src/java/logger/src/main/java/com/renren/dp/xlog/fs/XLogReader.java
+++ src/java/logger/src/main/java/com/renren/dp/xlog/fs/XLogReader.java
@@ -3,9 +3,6 @@ package com.renren.dp.xlog.fs;
 import java.io.Closeable;
 import java.io.IOException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * the line reader from the xlog input stream
  * <p>
@@ -24,24 +21,7 @@ import org.slf4j.LoggerFactory;
  * @author Zhancheng Deng {@mailto: zhancheng.deng@renren-inc.com}
  * @since 3:57:40 PM Mar 5, 2013
  */
-public class XLogReader implements Closeable {
-
-  private static final Logger LOG = LoggerFactory.getLogger(XLogReader.class);
-
-  private DispatcherClusterReader client;
-
-  /**
-   * the xlog reader constructor
-   * 
-   * @param categories
-   * @param zkConnString
-   * @param options
-   * @throws IOException
-   */
-  public XLogReader(String[] categories, String zkConnString, XLogConfiguration conf) throws IOException {
-    client = new DispatcherClusterReader(categories, zkConnString, conf);
-    LOG.info("initialize the xlogInputStream successfully.");
-  }
+public interface XLogReader extends Closeable {
 
   /**
    * the value returned has: <code>null</code>, the empty string <code>""</code>
@@ -53,13 +33,5 @@ public class XLogReader implements Closeable {
    * @return
    * @throws IOException
    */
-  public String readLine() throws IOException {
-    return client.readLine();
-  }
-
-  @Override
-  public void close() throws IOException {
-    client.close();
-  }
-
+  public String readLine() throws IOException;
 }
